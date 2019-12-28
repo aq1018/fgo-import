69832afec56e0b33a1c4331d4c04663babde1679
Aaron Qian <aq1018@gmail.com>
2019-12-28 00:00:47 -0800

    rename to fgo-import and add cli flags

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
 #![warn(clippy::all)]
 
+use clap::{App, Arg};
 use postgres::{Client, NoTls};
 
 mod data;
 use data::Fgo;
 
 fn main() {
-    let fgo = Fgo::from_file("./data/fgo.json").unwrap();
+    let matches = App::new("fgo-import")
+        .version("0.1")
+        .about("Load FGO data from a JSON file and export to PostgreSQL database.")
+        .author("Aaron Q.")
+        .arg(
+            Arg::with_name("dburl")
+                .short("d")
+                .long("dburl")
+                .value_name("URL")
+                .help("specify postgresql database URL")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("FILE")
+                .help("specify JSON file that contains FGO data.")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
 
-    let mut client = Client::connect("postgres://aqian@localhost/aqian", NoTls).unwrap();
+    let dburl = matches.value_of("dburl").unwrap();
+    let input = matches.value_of("input").unwrap();
+    let fgo = Fgo::from_file(input).unwrap();
+    let mut client = Client::connect(dburl, NoTls).unwrap();
+
     Fgo::reset_schema(&mut client).unwrap();
     fgo.save(&mut client).unwrap();
 }
